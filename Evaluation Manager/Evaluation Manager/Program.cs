074b579dfd5b3b3424fd1e5376b33130e79aa459
mbourlier-foi <mbourlier23@student.foi.hr>
2023-05-16 00:20:52 +0200

    Referenced Database Library

--- Evaluation Manager/Evaluation Manager/Program.cs
+++ Evaluation Manager/Evaluation Manager/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Evaluation_Manager
         [STAThread]
         static void Main()
         {
+            DB.SetConfiguration("PI2023_my-base_DB", "my_username", "my_password");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmLogin());
